ea6b6de2bf575f9499ab7fed850fa56462d18216
rroei-levi <rroei.levi@gmail.com>
2017-02-06 14:43:12 +0200

    Added the ability to add image from gallery

--- app/src/main/java/com/rlsoftware/mynight/activity_add_nightclub.java
+++ app/src/main/java/com/rlsoftware/mynight/activity_add_nightclub.java
@@ -1,6 +1,9 @@
 package com.rlsoftware.mynight;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +20,9 @@ public class activity_add_nightclub extends AppCompatActivity {
 
     Button btnSaveNightClub, ClubImageButton;
 
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,7 @@ public class activity_add_nightclub extends AppCompatActivity {
         clubImagePreView = (ImageView) findViewById(R.id.clubImagePreView);
 
         btnSaveNightClub.setOnClickListener(saveBtnListener);
+        ClubImageButton.setOnClickListener(addImageListener);
     }
 
 
@@ -57,4 +64,32 @@ public class activity_add_nightclub extends AppCompatActivity {
         }
 
     };
+
+    private View.OnClickListener addImageListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+           openGallery();
+
+        }
+    };
+
+    private void openGallery() {
+
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            clubImagePreView.setImageURI(imageUri);
+        }
+
+    }
+
 }
